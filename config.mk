58734afd16cb80bf305a40bb25e79529a163069e
TogoFire <italomellopereira@gmail.com>
2021-02-15 21:29:18 -0300

    ep: prebuilt apps

---
+++ config.mk
@@ -0,0 +1,119 @@
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := vendor/ExclusivePack
+
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/system/etc/permissions/com.sony.device.xml:system/etc/permissions/com.sony.device.xml \
+#    $(LOCAL_PATH)/system/etc/permissions/com.sony.sysinfo.xml:system/etc/permissions/com.sony.sysinfo.xml \
+#    $(LOCAL_PATH)/system/etc/permissions/com.sonyericsson.system.xml:system/etc/permissions/com.sonyericsson.system.xml \
+#    $(LOCAL_PATH)/system/framework/com.sony.device.jar:system/framework/com.sony.device.jar \
+#    $(LOCAL_PATH)/system/framework/com.sony.sysinfo.jar:system/framework/com.sony.sysinfo.jar \
+#    $(LOCAL_PATH)/system/framework/com.sonyericsson.system.jar:system/framework/com.sonyericsson.system.jar \
+
+#    $(LOCAL_PATH)/system/etc/ds1-default.xml:system/etc/ds1-default.xml \
+#    $(LOCAL_PATH)/system/etc/permissions/android.dolby.xml:system/etc/permissions/android.dolby.xml \
+#    $(LOCAL_PATH)/system/framework/dolby_ds.jar:system/framework/dolby_ds.jar
+
+# Cerberus
+#PRODUCT_PACKAGES += \
+#    Cerberus
+
+# SwiftKey
+#PRODUCT_PACKAGES += \
+#    SwiftKey
+
+# SemcMusic
+PRODUCT_PACKAGES += \
+    SemcMusic
+
+# NewPipe
+#PRODUCT_PACKAGES += \
+#    NewPipe
+
+# Ymusic
+#PRODUCT_PACKAGES += \
+#    Ymusic
+
+# FDroid
+#PRODUCT_PACKAGES += \
+#    FDroid
+
+# ColorNote
+PRODUCT_PACKAGES += \
+    ColorNote
+
+# SwiftBackup
+#PRODUCT_PACKAGES += \
+#    SwiftBackup
+
+# Termux
+#PRODUCT_PACKAGES += \
+#    Termux
+
+# DigiCal
+#PRODUCT_PACKAGES += \
+#    DigiCal
+
+# HedgeCam2
+#PRODUCT_PACKAGES += \
+#    HedgeCam2
+
+#OpenCamera
+PRODUCT_PACKAGES += \
+    OpenCamera
+
+# Gcamgo
+#PRODUCT_PACKAGES += \
+#    Gcamgo
+
+# Ds
+#PRODUCT_PACKAGES += \
+#    Ds
+
+# DsUI
+#PRODUCT_PACKAGES += \
+#    DsUI
+
+# Spectrum
+#PRODUCT_PACKAGES += \
+#    Spectrum
+
+# Cpl
+#PRODUCT_PACKAGES += \
+#    Cpl
+
+# Mi Remote Controller
+#PRODUCT_PACKAGES += \
+#    MiRemoteController
+
+# Hyperion
+#PRODUCT_PACKAGES += \
+#    Hyperion
+
+# SimpleGallery
+#PRODUCT_PACKAGES += \
+#    SimpleGallery
+
+# RevengeOSCalculator
+PRODUCT_PACKAGES += \
+    RevengeOSCalculator
+
+# NineEmail
+PRODUCT_PACKAGES += \
+    NineEmail
+
+# Textra
+#PRODUCT_PACKAGES += \
+#		Textra
